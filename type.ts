4f7e8cfa4e854d4ab1334562cfb7723bbff18028
pkumar002 <pkumar2@pythian.com>
2023-07-27 12:06:32 +0530

    Update types

--- type.ts
+++ type.ts
@@ -14,7 +14,7 @@ declare module NodeJS {
 
 interface SelectProps<T> {
   options: T[];
-  defaultValues?: string | T | T[];
+  defaultValues?: T[];
   keyExtractor?: (option: T) => string;
   getOptionLabel?: (option: T) => string;
   setValues?: (name: string, option: T | T[]) => void;
